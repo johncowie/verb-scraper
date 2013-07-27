f674c87f26d6378fd348aa3d1ecf0013847119cc
John Cowie <john.a.cowie@gmail.com>
2013-07-27 12:59:14 +0100

    Tidied up the algorithm

--- verbscraper.py
+++ verbscraper.py
@@ -6,19 +6,14 @@ soup = BeautifulSoup(website)
 
 table = soup.table
 trs = list(soup.table.find_all('tr'))
-trsfiltered = []
 headTds = ['tenses', 'english', '1s', '2s', '3s', '1p', '2p', '3p']
 
 for tr in trs:
 	tds = list(tr.find_all('td'))
 	if tds[0]['class'][0] == 'tense_heading':
-		trsfiltered.append(tr)
-
-for tr in trsfiltered:
-	tds = list(tr.find_all('td'))
-	print re.sub('\W+', '-', tds[0].text.lower()) + ':'
-	for i in range(1, len(tds)):
-		if len(re.sub('\W+', '', tds[i].text)) != 0:
-			print "\t" + headTds[i] + ": " + tds[i].text.replace('OR', ' / ')
+		print re.sub('\W+', '-', tds[0].text.lower()) + ':'
+		for i in range(1, len(tds)):
+			if len(re.sub('\W+', '', tds[i].text)) != 0:
+				print "\t" + headTds[i] + ": " + tds[i].text.replace('OR', ' / ')
 
 
